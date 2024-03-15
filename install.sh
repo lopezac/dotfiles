f0e4ccc44b6f8a547fd8022739f2c6a5f324a5e2
Axel C. Lopez <lopezaxel@protonmail.com>
2024-03-15 18:02:12 -0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# install basic packages
+sudo dnf install git cargo make python3-pip vim neovim
+
+# install node with nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install node
+
+# config git
+git config --global user.email "lopezaxel@protonmail.com"
+git config --global user.name "Axel C. Lopez"
+
+# install lunarvim
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+
+# install alacritty config
+cp alacritty/alacritty.toml ~/.alacritty.toml
+
+# install tmux config
+cp tmux/.tmux.conf ~/.tmux.conf
+
+# install lunarvim config
+if ! ls ~/.config/lvim; then
+  mkdir ~/.config/lvim
+fi
+cp lunarvim/* ~/.config/lvim/
+
+# install fonts
+if ! ls ~/.local/share/fonts; then
+  mkdir ~/.local/share/fonts
+fi
+cp fonts/* ~/.local/share/fonts
+sudo fc-cache -v
+
+# install bashrc config
+cat bash/.bashrc >> ~/.bashrc
