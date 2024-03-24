9f81fa438dc70dacb73c33b17294226e6dd6c556
Axel C. Lopez <lopezaxel@protonmail.com>
2024-03-24 15:29:41 -0300

    Change rust installation, remove folders, add git config

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # install basic packages
-sudo dnf install -y git cargo make python3-pip vim neovim ripgrep alacritty
+sudo dnf install -y git make python3-pip vim neovim ripgrep alacritty clang
+
+# install rust (cargo package)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # install node with nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
@@ -13,28 +16,29 @@ nvm install node
 # config git
 git config --global user.email "lopezaxel@protonmail.com"
 git config --global user.name "Axel C. Lopez"
+git config --global pull.rebase false # prefer merge instead of rebase
 
 # install lunarvim
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
 # install alacritty config
-cp alacritty/alacritty.toml ~/.alacritty.toml
+cp ./alacritty/alacritty.toml ~/.alacritty.toml
 
 # install tmux config
-cp tmux/.tmux.conf ~/.tmux.conf
+cp ./tmux.conf ~/.tmux.conf
 
 # install lunarvim config
-if ! [ -d ~/.config/lvim ]; then
+if ! [[ -d ~/.config/lvim ]]; then
   mkdir ~/.config/lvim
 fi
-cp -r lunarvim/* ~/.config/lvim/
+cp -r ./neovim/* ~/.config/lvim/
 
 # install fonts
-if ! [ -d ~/.local/share/fonts ]; then
+if ! [[ -d ~/.local/share/fonts ]]; then
   mkdir ~/.local/share/fonts
 fi
 cp font/* ~/.local/share/fonts
 sudo fc-cache -v
 
 # install bashrc config
-cat bash/.bashrc >> ~/.bashrc
+cat bashrc >> ~/.bashrc
