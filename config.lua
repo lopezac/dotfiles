a7cf94314420234a9e425b043a34074593a73b75
Axel C. Lopez <lopezaxel@protonmail.com>
2024-03-25 13:23:00 -0300

    Revert format filetypes to default

---
+++ config.lua
@@ -0,0 +1,32 @@
+local natural_cmp = require "recipes"
+local formatters = require "lvim.lsp.null-ls.formatters"
+
+formatters.setup {
+  { name = "autopep8" },
+  { name = "prettier" },
+}
+
+lvim.plugins = {
+  { "xiyaowong/virtcolumn.nvim" },
+  { "projekt0n/github-nvim-theme" },
+}
+
+lvim.colorscheme = "github_dark_high_contrast"
+-- Add ruler at column 80 with virtcolumn.nvim plugin
+vim.opt.colorcolumn = "80"
+
+-- Set tabs size for different files
+lvim.autocommands = {
+  {
+    "BufEnter",
+    {
+      pattern = { "*.c", "*.cpp", "*.py" },
+      command = "setlocal tabstop=4 shiftwidth=4",
+    }
+  },
+}
+
+-- Use natural sort in nvim-tree
+lvim.builtin.nvimtree.setup.sort_by = function(nodes)
+  table.sort(nodes, natural_cmp)
+end
